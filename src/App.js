7d760a06140f17d4cdaacb5171d7c391affe0119
kokis128 <jorgevalentini76@gmail.com>
2022-09-04 10:12:07 -0300

    create component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+
+
+
+
+import Navigation from './components/Navigation'
+import CreateNote from './components/CreateNote'
+import CreateUser from './components/CreateUser'
+import {NoteList} from './components/NoteList'
 
 
 
 function App() {
   return (
+   
+    
 
-    <div>
-      hello world
-    </div>
+    
+  
+     
+    
+    
+   
 
+<Router>
+
+<Navigation/>
+    
+    
+    
+    <Routes>  
+      
+      
+      <Route path="/" exact element={<NoteList/>}   />
+      <Route  path="/edit/:id" element={<CreateNote/>}/>
+      <Route path="/create" element={<CreateNote/>}  />
+      <Route path="/user" element={<CreateUser/>}  />
+     
+
+    </Routes>
+
+    </Router>
+
+   
+     
+     
+    
+    
+
+    )
 
  
-  );
+
+  
+
+ 
+  
 }
 
 export default App;
---
+++ src/components/NoteList.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const NoteList = () => {
+  return (
+    <div>NoteList</div>
+  )
+}
+

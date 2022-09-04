bb4ecff6ac8ce6be46f4a88c98d307f9e581c71f
kokis128 <jorgevalentini76@gmail.com>
2022-09-04 16:41:32 -0300

    component and rutas and navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom'
+import { Navbar } from 'react-bootstrap'
 
 
 
@@ -9,6 +10,7 @@ import Navigation from './components/Navigation'
 import CreateNote from './components/CreateNote'
 import CreateUser from './components/CreateUser'
 import {NoteList} from './components/NoteList'
+
 
 
 
@@ -30,16 +32,25 @@ function App() {
     
     
     
+     <div className='container p-4'>
     <Routes>  
       
-      
-      <Route path="/" exact element={<NoteList/>}   />
-      <Route  path="/edit/:id" element={<CreateNote/>}/>
-      <Route path="/create" element={<CreateNote/>}  />
-      <Route path="/user" element={<CreateUser/>}  />
+
+       
+     <Route path="/" element={<Navbar.Brand />}   />
+     
+        <Route path="/" element={<NoteList/>}   />
+        <Route  path="/edit/:id" element={<CreateNote/>}/>
+        <Route path="/create" element={<CreateNote/>}  />
+        <Route path="/user" element={<CreateUser/>}  />
+        <Route path="*" element={<Navigate replace to = "/"/>}  />        
+
+      <Route/>
+
      
 
     </Routes>
+      </div>
 
     </Router>
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,44 @@
 import React, { Component } from 'react'
 
+
+
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Outlet, Link} from 'react-router-dom'
+
+
+
 export default class Navigation extends Component {
   render() {
     return (
-      <div>Navigation</div>
+
+      <>
+
+<Navbar className="navBg" variant="dark" bg="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as = {Link} to = "/">Notas App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as = {Link} to = "/">Home</Nav.Link>
+            <Nav.Link as = {Link} to = "create">Notas</Nav.Link>
+            <Nav.Link as = {Link} to = "edit/:id">Modificar Nota</Nav.Link>
+            <Nav.Link as = {Link} to = "user">Crear Usuario</Nav.Link>
+           
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    <section>
+      <Outlet>
+
+      </Outlet>
+    </section>
+      
+      
+      
+      </>
+      
     )
   }
 }
